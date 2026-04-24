a425d0752f224b6c3a2212c2b84d9709f42c3750
anjalirawat2717 <anjalirawat2717@gmail.com>
2026-04-24 12:37:04 +0530

    setted routes and created header

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,22 +4,22 @@ import { assets } from '../../assets/assets'
 
 const Navbar = () => {
 
-   const[menu,setmenu] =useState("Home");
+  const [menu, setmenu] = useState("Home");
 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
-     <ul className="navbar-menu">
-    <li onClick={ ()=>setmenu("Home")}className={menu==='Home'?'active':""}>Home</li>
-    <li onClick={ ()=>setmenu("menu")}className={menu==='menu'?'active':""}>menu</li>
-    <li onClick={ ()=>setmenu("mobile-app")}className={menu==='mobile-app'?'active':""}>mobile-app</li>
-    <li onClick={ ()=>setmenu("contact us")}className={menu==='contact us'?'active':""}>contact us</li>
-       </ul> 
+      <ul className="navbar-menu">
+        <li onClick={() => setmenu("Home")} className={menu === 'Home' ? 'active' : ""}>Home</li>
+        <li onClick={() => setmenu("menu")} className={menu === 'menu' ? 'active' : ""}>menu</li>
+        <li onClick={() => setmenu("mobile-app")} className={menu === 'mobile-app' ? 'active' : ""}>mobile-app</li>
+        <li onClick={() => setmenu("contact us")} className={menu === 'contact us' ? 'active' : ""}>contact us</li>
+      </ul>
       <div className='navbar-right'>
-      <img src={assets.search_icon} alt=''/>
+        <img src={assets.search_icon} alt='' />
         <div className='navbar-search-icon'>
-               <img src={assets.basket_icon} alt=''/>
-                   <div className='dot'></div>
+          <img src={assets.basket_icon} alt='' />
+          <div className='dot'></div>
         </div>
         <button>sign in</button>
       </div>
