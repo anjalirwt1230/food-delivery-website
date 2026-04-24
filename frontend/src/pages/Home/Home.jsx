cfa7919aebe11d43c52028aea5152870e1be5232
anjalirawat2717 <anjalirawat2717@gmail.com>
2026-04-24 15:42:53 +0530

    Explore menu created

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header.jsx'
+import Exploremenu from '../../components/Exploremenu/Exploremenu.jsx'
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Exploremenu/>
     </div>
   )
 }
